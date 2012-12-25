c01ee35a6b94346238231eff93d891bd19ce0f0d
p2pmsg <p2pcmp@p2pmsgtekiiMac.local>
2012-12-25 14:37:40 +0800

    Initial Commit

---
+++ P2PBOX/PAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  PAppDelegate.h
+//  P2PBOX
+//
+//  Created by p2pmsg on 12-12-25.
+//  Copyright (c) 2012年 p2pmsg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ P2PBOX/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  P2PBOX
+//
+//  Created by p2pmsg on 12-12-25.
+//  Copyright (c) 2012年 p2pmsg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PAppDelegate class]));
+    }
+}
